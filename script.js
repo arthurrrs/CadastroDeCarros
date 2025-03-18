7d39d20b3f08fd21d9f480023a978b1da05d2017
Arthur Rodrigues <143718272+arthurrrs@users.noreply.github.com>
2025-03-18 10:54:37 -0300

    car

--- script.js
+++ script.js
@@ -13,11 +13,17 @@ async function fetchData() {
         tableBody.innerHTML = '';
 
         cars.reverse().forEach(function(car) {
+          const fuelText = getFuelName(car.gasType);
+
           const row = `
             <tr data-id="${car.id}">
             <td>${car.brand}</td>
             <td>${car.name}</td>
             <td>${car.price}</td>
+            <td>${fuelText}</td>
+            <td>
+            <button class="btn btn-danger btn-sm" id="delete-btn" data-id="${car._id}">Deletar</button>
+            </td>
           `;
           tableBody.innerHTML += row;
         })
@@ -27,6 +33,55 @@ async function fetchData() {
 
     }
     
+    function getFuelName(value) {
+      const fuelType = {
+        "0" : "Desconhecido",
+        "1" : "Gasolina",
+        "2" : "Etanol + Gasolina",
+        "3" : "Eletricidade",
+        "4" : "Etanol",
+      };
+      return fuelType[value] || "Desconhecido"
+      
+    }
+
+    document.addEventListener('click', function(event) {
+      if (event.target.id.startsWith('delete-btn')) {
+        const carId = event.target.getAttribute('data-id');
+
+      deleteCar(carId);
+
+      }
+    }
+  )
+
+
+
+    async function deleteCar(carId) {
+      try {
+        const response = await fetch(`https://carangas.herokuapp.com/cars/${carId}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "aplication/json"
+        }
+        });
+         
+          if (!response.ok) {
+            throw new Error("Erro ao Deletar o carro");
+          }
+
+          console.log("Carro deletado com sucesso!");
+          
+          location.reload()
+
+      } catch (error) { 
+        console.error(error);
+        
+      }
+      
+    }
+
+
 }
 
 fetchData()
